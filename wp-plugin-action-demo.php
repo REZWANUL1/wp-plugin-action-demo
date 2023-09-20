807decccd5b01336a3966c080b5e2f185af6b350
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2023-09-20 06:05:11 +0600

    adding redirect function

--- wp-plugin-action-demo.php
+++ wp-plugin-action-demo.php
@@ -17,8 +17,42 @@
 if (!defined('ABSPATH')) {
    exit;
 }
-function load_my_plugin_translation()
+function wad_load_my_plugin_translation()
 {
    load_plugin_textdomain('your-plugin-textdomain', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 }
-add_action('plugins_loaded', 'load_my_plugin_translation');
+add_action('plugins_loaded', 'wad_load_my_plugin_translation');
+
+//?Register a custom menu page.
+function wad_menu_page()
+{
+   add_menu_page(
+      __('Wad Menu Title', 'wad'),
+      __('Wad items', 'wad'),
+      'manage_options',
+      'wad_custompage',
+      'wad_custom_menu_page',
+      'dashicons-plugins-checked'
+   );
+}
+add_action('admin_menu', 'wad_menu_page');
+
+
+// wad  Display a custom menu page
+
+function wad_custom_menu_page()
+{
+   esc_html_e('Wad Admin Page Test', 'wad');
+}
+
+// Redirect user to plugin options page
+
+add_action('activated_plugin', 'wad_redirect_plugin_page');
+
+function wad_redirect_plugin_page($plugin)
+{
+   if (plugin_basename(__FILE__) == $plugin) {
+      wp_redirect(admin_url('admin.php?page=wad_custompage'));
+      die();
+   }
+}
